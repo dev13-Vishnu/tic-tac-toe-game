654efbd0b868ebc3e4b9d5be78126ae07172ac56
Vishnu P S <vishnu13three@gamil.com>
2025-08-06 20:48:48 +0530

    started to implement timetravel

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ function Square({value, onSquareClick}){
 
   return <button className="square" onClick={onSquareClick} >{value}</button>
 }
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
+function Board({xIsNext, squares, onPlay}) {
   function handleClick (i) {
     if(squares[i] || calculateWinner(squares)){
       return;
@@ -19,8 +16,7 @@ function Board() {
     } else {
       nextSquares[i] = "O"
     }
-    setSquares (nextSquares)
-    setXIsNext(!xIsNext);
+    onPlay (nextSquares)
   }
   const winner = calculateWinner(squares);
   let status;
@@ -51,6 +47,43 @@ function Board() {
     </>
   )
 }
+function Game () {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSqures = history[history.length -1]
+
+  const handlePlay = (nextSquares) => {
+    setHistory([...history,nextSquares])
+    setXIsNext(!xIsNext);
+  }
+  function jumpTo(nextMove){
+
+  }
+  const moves = history.map((squares,move) => {
+    let description;
+    if(move> 0){
+      description  = "Go to move #" + move;
+    } else {
+      description = "Go to Game start";
+    }
+    return (
+      <li key = {move}>
+        <button onClick={() => {jumpTo(move)}}>{description}</button>
+      </li>
+    )
+  })
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext = {xIsNext} squares= {currentSqures} onPlay = {handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
 function calculateWinner(squares) {
   let lines = [
     [0, 1, 2],
@@ -70,4 +103,4 @@ function calculateWinner(squares) {
   }
   return null;
 }
-export default Board
+export default Game
